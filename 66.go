51e78e8f95628e6cbdc03a6049044e0fd09c0e9b
Kochiev.Alan <Kochiev.Alan@wildberries.ru>
2020-05-06 02:18:11 +0300

    fix

--- 66.go
+++ 66.go
@@ -1,17 +1,13 @@
 package main
 
 func plusOne(digits []int) []int {
-	value := 1
 	for a := len(digits) - 1; a >= 0; a-- {
-		if value == 1 {
-			digits[a] ++
-			value = digits[a] / 10
-			digits[a] = digits[a] % 10
-		}else{
+		digits[a] ++
+		if digits[a] < 10 {
 			return digits
 		}
+		digits[a] = 0
 	}
 	a := []int{1}
-	return append(a,digits ...)
+	return append(a, digits...)
 }
-
