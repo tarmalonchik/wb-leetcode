5f15cf3590c1e4c72361653976ec38571f38950e
Kochiev.Alan <Kochiev.Alan@wildberries.ru>
2020-05-06 02:02:24 +0300

    bag

--- 66.go
+++ 66.go
@@ -1,20 +1,17 @@
 package main
 
 func plusOne(digits []int) []int {
-	result := make([]int, len(digits) + 1)
 	value := 1
 	for a := len(digits) - 1; a >= 0; a-- {
 		if value == 1 {
-			digits[a] += value
+			digits[a] ++
 			value = digits[a] / 10
 			digits[a] = digits[a] % 10
+		}else{
+			return digits
 		}
-		result[a+1] = digits[a]
 	}
-	if result[1] == 0 {
-		result[0] = 1
-		return result
-	}
-	return result[1:]
+	a := []int{1}
+	return append(a,digits ...)
 }
 
