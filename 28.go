229522e6c07b1aa6108db29e585f05ff0a9edf81
Kochiev.Alan <Kochiev.Alan@wildberries.ru>
2020-05-06 17:31:10 +0300

    making task

---
+++ 28.go
@@ -0,0 +1,13 @@
+package main
+
+func strStr(haystack string, needle string) int {
+	if len(needle) > len(haystack){
+		return -1
+	}
+	for i := 0; i <= len(haystack) - len(needle); i++{
+		if haystack[i:i+ len(needle)] == needle{
+			return i
+		}
+	}
+	return -1
+}
