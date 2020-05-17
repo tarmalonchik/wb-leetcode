6268807345493a646d4256b8e74ca51a9939f9a1
Kochiev.Alan <Kochiev.Alan@wildberries.ru>
2020-05-17 16:56:26 +0300

    e

--- 7.go
+++ 7.go
@@ -3,11 +3,15 @@ package main
 func reverse(x int) int {
 	result := 0
 	for {
-		if x/10 == 0{
-			return result*10 + x%10
+		if x/10 == 0 {
+			answer := result*10 + x
+			if answer > 2147483647 || answer < -2147483648 {
+				return 0
+			}
+			return answer
 		}
 		result *= 10
 		result += x % 10
-		x = x/10
+		x = x / 10
 	}
 }
