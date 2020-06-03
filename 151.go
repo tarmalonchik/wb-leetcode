76be75498fe283ac513b115ec7e2bf32f8b566b0
Kochiev.Alan <Kochiev.Alan@wildberries.ru>
2020-06-04 00:29:11 +0300

    151

---
+++ 151.go
@@ -0,0 +1,32 @@
+package main
+
+func reverseWords(s string) string {
+	num := len(s)
+	if num <= 0 {
+		return ""
+	}
+	str := ""
+	flag := true
+	for i := len(s) - 1; i >= 0; i-- {
+		if s[i] == ' ' && flag {
+			num = i
+			continue
+		}
+		if s[i] == ' ' {
+			str += s[i+1:num+1] + " "
+			flag = true
+		} else {
+			if flag == true {
+				num = i
+			}
+			flag = false
+		}
+	}
+	if s[0] != ' '{
+		return str + s[:num+1]
+	}
+	if len(str) > 0{
+		return str[:len(str)-1]
+	}
+	return ""
+}
