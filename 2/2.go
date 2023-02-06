407237dbfda90dd9c95d0eafc81e4e6db9842038
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2023-02-06 18:46:57 -0500

    to folders

--- 2/2.go
+++ 2/2.go
@@ -1,9 +1,9 @@
 package main
 
-//type ListNode struct {
-//	Val  int
-//	Next *ListNode
-//}
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 	flag := 0
