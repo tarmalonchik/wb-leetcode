10c05363557f73e7c2ab23e53420f75399641072
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2023-02-06 21:11:51 -0500

    fxi

---
+++ 83/main.go
@@ -0,0 +1,29 @@
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	floatingNode := head
+	for {
+		if floatingNode.Next == nil {
+			break
+		}
+		if floatingNode.Next.Val == floatingNode.Val {
+			floatingNode.Next = floatingNode.Next.Next
+		} else {
+			floatingNode = floatingNode.Next
+		}
+	}
+	return head
+}
