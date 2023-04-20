b6988899276950d4024fed5d95bb48e4c3add282
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2023-04-19 18:02:41 -0600

    fix

---
+++ 12/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type romans struct {
+	num   int
+	roman string
+}
+
+var list = []romans{
+	{
+		num:   1000,
+		roman: "M",
+	},
+	{
+		num:   900,
+		roman: "CM",
+	},
+	{
+		num:   500,
+		roman: "D",
+	},
+	{
+		num:   400,
+		roman: "CD",
+	},
+	{
+		num:   100,
+		roman: "C",
+	},
+	{
+		num:   90,
+		roman: "XC",
+	},
+	{
+		num:   50,
+		roman: "L",
+	},
+	{
+		num:   40,
+		roman: "XL",
+	},
+	{
+		num:   10,
+		roman: "X",
+	},
+	{
+		num:   9,
+		roman: "IX",
+	},
+	{
+		num:   5,
+		roman: "V",
+	},
+	{
+		num:   4,
+		roman: "IV",
+	},
+	{
+		num:   1,
+		roman: "I",
+	},
+}
+
+func main() {
+	fmt.Println(intToRoman(14))
+}
+
+func intToRoman(num int) string {
+	var resp string
+	for i := range list {
+		val := num / list[i].num
+		resp += strings.Repeat(list[i].roman, val)
+		num = num % list[i].num
+	}
+	return resp
+}
