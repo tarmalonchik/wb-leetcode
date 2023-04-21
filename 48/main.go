efb91e4a7da495b94c253185ad9e3e2c23b1f16a
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2023-04-21 14:14:13 -0600

    fix

--- 48/main.go
+++ 48/main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-func main() {
-	matrix := [][]int{
-		{1, 2, 3, 4, 5},
-		{6, 7, 8, 9, 10},
-		{11, 12, 13, 14, 15},
-		{16, 17, 18, 19, 20},
-		{21, 22, 23, 24, 25},
-	}
-	printMx(matrix)
-	rotate(matrix)
-	printMx(matrix)
-}
-
 func rotate(matrix [][]int) {
 	if len(matrix) == 0 || len(matrix) == 1 {
 		return
@@ -26,27 +9,14 @@ func rotate(matrix [][]int) {
 			if i == len(matrix)-1-j {
 				break
 			}
-			first := matrix[0][i]
-			matrix[0][i] = matrix[len(matrix)-1-i][0]
-			matrix[len(matrix)-1-i][0] = matrix[len(matrix)-1][len(matrix)-1-i]
-			matrix[len(matrix)-1][len(matrix)-1-i] = matrix[i][len(matrix)-1]
-			matrix[i][len(matrix)-1] = first
+			first := matrix[j][i]
+			matrix[j][i] = matrix[len(matrix)-1-i][j]
+			matrix[len(matrix)-1-i][j] = matrix[len(matrix)-1-j][len(matrix)-1-i]
+			matrix[len(matrix)-1-j][len(matrix)-1-i] = matrix[i][len(matrix)-1-j]
+			matrix[i][len(matrix)-1-j] = first
 		}
 		if j+1 >= len(matrix)/2 {
 			return
 		}
 	}
-}
-
-func printMx(in [][]int) {
-	for i := range in {
-		fmt.Println()
-		for j := range in[i] {
-			fmt.Print(in[i][j])
-			if j != len(in[i])-1 {
-				fmt.Print("-")
-			}
-		}
-	}
-	fmt.Println()
 }
