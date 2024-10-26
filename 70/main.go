65d1f1ee274822ad1d613d1efc6df72d2957fa5a
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2024-10-26 22:06:25 +0200

    70

--- 70/main.go
+++ 70/main.go
@@ -1,13 +1,27 @@
 package main
 
-import (
-	"fmt"
-)
-
-func main() {
-	fmt.Println(climbStairs(10))
+func climbStairs(n int) int {
+	sto := storage{}
+	for i := 0; i < n; i++ {
+		sto.calculate()
+	}
+	return sto.sum
 }
 
-func climbStairs(n int) int {
-	return 0
+type storage struct {
+	finishWithOne int
+	finishWithTwo int
+	sum           int
+}
+
+func (s *storage) calculate() {
+	if s.sum == 0 {
+		s.finishWithOne++
+		s.sum++
+		return
+	}
+	s.sum = s.finishWithOne + s.finishWithTwo
+	s.sum += s.finishWithOne
+	s.finishWithOne = s.finishWithOne + s.finishWithTwo
+	s.finishWithTwo = s.finishWithOne - s.finishWithTwo
 }
