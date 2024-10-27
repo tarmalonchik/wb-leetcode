fe49aa185fc2baffc577f4e39a8c8976b694de70
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2024-10-27 14:08:59 +0100

    198

---
+++ 198/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	input := []int{2, 7, 9, 3, 1}
+	fmt.Println(rob(input))
+}
+
+func rob(nums []int) int {
+	s := storage{}
+	for i := range nums {
+		s.addNum(nums[i])
+	}
+	return s.total
+}
+
+type storage struct {
+	total    int
+	decIfAdd int
+}
+
+func (s *storage) addNum(num int) {
+	if num == 0 {
+		s.decIfAdd = 0
+		return
+	}
+	if s.total == 0 {
+		s.total += num
+		s.decIfAdd = num
+		return
+	}
+	if s.decIfAdd == 0 {
+		s.total += num
+		s.decIfAdd = num
+		return
+	}
+	if s.total+num-s.decIfAdd > s.total {
+		s.total = s.total + num - s.decIfAdd
+		s.decIfAdd = num - s.decIfAdd
+		return
+	} else {
+		s.decIfAdd = 0
+	}
+}
