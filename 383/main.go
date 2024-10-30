d451a77a35fd34508a73632a75591577b57f7322
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2024-10-30 15:31:10 +0100

    383

---
+++ 383/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(canConstruct("1", "1"))
+}
+
+func addToMap(mp map[uint8]uint32, val uint8) {
+	mp[val]++
+}
+
+func getFromMap(mp map[uint8]uint32, val uint8) bool {
+	count, ok := mp[val]
+	if !ok {
+		return false
+	}
+	if count == 0 {
+		return false
+	}
+	mp[val]--
+	return true
+}
+
+func canConstruct(ransomNote string, magazine string) bool {
+	mp := make(map[uint8]uint32, len(magazine))
+	for i := range magazine {
+		addToMap(mp, magazine[i])
+	}
+
+	for i := range ransomNote {
+		if !getFromMap(mp, ransomNote[i]) {
+			return false
+		}
+	}
+	return true
+}
