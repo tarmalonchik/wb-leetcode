175484c522abfb8fc343bca83689320bc2bb663e
Alan Kochiev <117481481+tarmalonchik@users.noreply.github.com>
2024-10-30 15:06:13 +0100

    137

--- 137/main.go
+++ 137/main.go
@@ -1,17 +1,58 @@
 package main
 
-import (
-	"fmt"
-)
+func singleNumber(nums []int) int {
+	var positiveCounter = make([]int, 64)
+	var negativeCounter = make([]int, 64)
+	for i := range nums {
+		if nums[i] >= 0 {
+			addNum(positiveCounter, nums[i])
+		} else {
+			addNum(negativeCounter, abs(nums[i]))
+		}
+	}
 
-func main() {
-	var x = 1
-	var y = 10
+	positive := getNumFromCounter(positiveCounter)
+	negative := getNumFromCounter(negativeCounter)
+	if positive != 0 {
+		return positive
+	}
+	return -negative
+}
 
-	x ^= y
-	y ^= x
-	x ^= y
+func abs(a int) int {
+	if a >= 0 {
+		return a
+	}
+	return -a
+}
 
-	fmt.Println(x)
-	fmt.Println(y)
+func getNumFromCounter(counter []int) (resp int) {
+	for i := len(counter) - 1; i >= 0; i-- {
+		if counter[i] == 0 {
+			continue
+		}
+		resp = setBit(resp, uint(len(counter)-1-i))
+	}
+	return resp
+}
+
+func addNum(counter []int, num int) {
+	for i := len(counter) - 1; i >= 0; i-- {
+		if hasBit(num, uint(len(counter)-1-i)) {
+			counter[i]++
+		}
+		if counter[i] == 3 {
+			counter[i] = 0
+		}
+	}
+}
+
+func setBit(n int, pos uint) int {
+	return n | (1 << pos)
+
+}
+
+func hasBit(n int, pos uint) bool {
+	val := n & (1 << pos)
+	return val > 0
 }
